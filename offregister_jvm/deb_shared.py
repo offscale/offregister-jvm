11e4ad358784b45abf45a7c28143cbc8dd412ba8
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-09-03 21:37:47 -0400

    [offregister_jvm/deb_shared.py] Enable specific versions to be installed ; add more documentation

--- offregister_jvm/deb_shared.py
+++ offregister_jvm/deb_shared.py
@@ -6,19 +6,37 @@ from offregister_fab_utils.apt import apt_depends
 logger = getLogger(modules[__name__].__name__)
 
 
-def install0(c, jvm="oracle", **kwargs):
+def install0(c, jvm="oracle", jvm_version=18, **kwargs):
     """
+    Install JVM
+
     :param c: Connection
     :type c: ```fabric.connection.Connection```
+
+    :param jvm: Which Java Virtual Machine to use
+    :type jvm: ```Literal['oracle', 'openjdk']```
+
+    :param jvm_version: Which JVM version to install
+    :type jvm_version: ```int```
+
+    :return: Install response
+    :rtype: ```Union[str,fabric.runners.Result]```
     """
     if jvm == "oracle":
         apt_depends(c, "curl")
-        deb_file = "jdk-18_linux-x64_bin.deb"
-        c.run("curl -OL https://download.oracle.com/java/18/latest/{}".format(deb_file))
-        return c.sudo("dpkg -i {}".format(deb_file))
+        deb_file = "jdk-{}_linux-x64_bin.deb".format(jvm_version)
+        c.run(
+            "curl -OL https://download.oracle.com/java/{}/latest/{}".format(
+                jvm_version, deb_file
+            )
+        )
+        res = c.sudo("dpkg -i {}".format(deb_file))
+        c.run("rm {}".format(deb_file))
+        return res
     elif jvm == "openjdk":
-        apt_depends(c, "openjdk-18-jdk")
-        return "Install OpenJDK"
+        apt_depends(c, "openjdk-{}-jdk".format(jvm_version))
+        return "Installed OpenJDK {}".format(jvm_version)
+    raise NotImplementedError(jvm)
 
 
 __all__ = ["install0"]
